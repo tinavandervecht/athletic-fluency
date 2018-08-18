0106d8edd6705dbe110eb8a5b5cec1c8021a524c
Tina Vandervecht <tvandervecht@gmail.com>
2018-08-17 23:16:34 -0400

    Hero section rough layout and styles.

--- application/themes/athletic_fluency_theme/elements/header.php
+++ application/themes/athletic_fluency_theme/elements/header.php
@@ -14,6 +14,7 @@ $u = new User;
 
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'/>
     <link href="<?php echo $this->getThemePath(); ?>/css/app.css" rel="stylesheet" type="text/css"/>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,900" rel="stylesheet">
 
     <script>
         if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
---
+++ application/themes/athletic_fluency_theme/elements/navigation.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="#" class="btn btn-link">About Us</a></li>
+        <li><a href="#" class="btn btn-link">Athletes</a></li>
+        <li><a href="#" class="btn btn-link">Classes</a></li>
+        <li><a href="#" class="btn btn-link">Personal Training</a></li>
+        <li><a href="#" class="btn btn-link">Contact Us</a></li>
+    </ul>
+</nav>
